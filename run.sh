44e44ddf75c350b6c8362949229e0bc915212803
Johannes Ring <johannr@simula.no>
2016-04-07 15:06:07 +0200

    WIP

--- run.sh
+++ run.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
 
-: ${WORKDIR=/data}
-
-BRANCH=$(echo "${bamboo_planRepository_branchName}" | sed 's|/|-|g')
-IMAGE=${bamboo_DOCKERIMAGE_BASENAME}-${BRANCH}
-REPO=${bamboo_DOCKER_REGISTRY_REPO}/${IMAGE}
-
 if [[ $# -lt 1 ]]; then
     echo "usage: ${0##*/} COMMAND"
     exit 1
 fi
-CMD=${1}
+DOCKER_CMD=${1}
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+. ${SCRIPT_DIR}/bamboo.conf
 
 # Make sure volumes for instant cache and ccache exists
-docker volume create --name instant-cache-${bamboo_DOCKERIMAGE_BASENAME}
+docker volume create --name instant-cache-${DOCKER_IMAGE_BASENAME}
 docker volume create --name ccache
 
-docker pull ${REPO}
+docker pull ${DOCKER_REPO}
 
 docker run \
        --volume ccache:/home/fenics/.ccache \
-       --volume instant-cache-${bamboo_DOCKERIMAGE_BASENAME}:/home/fenics/.instant \
-       --volume ${bamboo_working_directory}:${WORKDIR} \
+       --volume instant-cache-${DOCKER_IMAGE_BASENAME}:/home/fenics/.instant \
+       --volume ${bamboo_working_directory}:${BAMBOO_WORKDIR} \
        --workdir /home/fenics \
        --rm \
-       ${REPO} \
-       ${CMD}
+       ${DOCKER_REPO} \
+       ${DOCKER_CMD}
